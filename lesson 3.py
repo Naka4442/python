7a357d02a0aea500cf6989467afb93ac30618224
Alex Chernov <nak4442@gmail.com>
2021-04-23 18:56:04 +0300

    Перебор списка

--- lesson 3.py
+++ lesson 3.py
@@ -1,17 +1,4 @@
-# count = int(input("Сколько чисел вы хотите ввести?"))
-# numbers = []
-# for i in range(count):
-#     numbers.append(int(input("Введите число >> ")))
-# newNumbers = []
-# for number in numbers:
-#     if number > 5:
-#         newNumbers.append(number)
-# print(newNumbers)
-
-
-letters = "пЛРФЫЩГЗФЩЦКрЗФЩЫТДФЛЫАиЗЩЫФЗвДЛВРАеЗЩФОЗЩАОАт"
-clean = ""
-for letter in letters:
-    if not letter.isupper():
-        clean += letter
-print(clean)
+#Перебор списка
+cities = ["Иваново", "Ярославль", "Кострома", "Нижний Новгород"]
+for city in cities:
+    print(city)
