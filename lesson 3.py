80856037898630b6dc5fcbd07faeb1554cf666ef
Alex Chernov <nak4442@gmail.com>
2021-04-25 16:51:08 +0300

    Изменил lesson 3

--- lesson 3.py
+++ lesson 3.py
@@ -1,4 +1,28 @@
 #Перебор списка
 cities = ["Иваново", "Ярославль", "Кострома", "Нижний Новгород"]
+citiesStr = ""
 for city in cities:
-    print(city)
+    citiesStr += city
+    if(cities.index(city) != len(cities) - 1):
+        citiesStr += ", "
+print(citiesStr)
+#Свойтсва и методы списков
+#Добавление элемента в список
+newCity = "Москва"
+cities.append(newCity)
+print(cities)
+#Добавление элемента на определенное место в списке 
+cities.insert(1, "Астрахань")
+print(cities)
+#Задача викторина
+q = []
+a = []
+count = int(input("Введите количество вопрос\n>> "))
+score = 0
+for i in range(count):
+    q.append(input("Введите вопрос\n>> "))
+    a.append(input("Введите ответ\n>> "))
+for i in range(count):
+    if(input(q[i]) == a[i]):
+        score += 1
+print("Вы ответили на ", score/count * 100, "% вопросов")
