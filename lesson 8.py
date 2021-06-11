76bde331280443b21c5ab3baa30552a8bf3703bd
Alex Chernov <nak4442@gmail.com>
2021-06-11 18:02:52 +0300

    8 занятие

--- lesson 8.py
+++ lesson 8.py
@@ -1,6 +1,7 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QToolTip, QPushButton, QLabel
+from PyQt5.QtGui import QFont, QIcon
+
 
 class Test(QWidget):
 
@@ -9,6 +10,20 @@ class Test(QWidget):
         self.initUI()
 
     def initUI(self):
+        QToolTip.setFont(QFont('Verdana', 10))
+
+        self.setToolTip("Hello World!")
+
+        button = QPushButton("Button", self)
+        button.setToolTip("Privet")
+        button.resize(button.sizeHint())
+        button.move(50, 50)
+
+        label = QLabel(self)
+        label.setText("Hello world!")
+        label.setFont(QFont("Gili Sans", 10))
+        label.move(25, 25)
+
         self.setGeometry(300, 300, 300, 220)
         self.setWindowTitle("Привет PyQt")
         self.setWindowIcon(QIcon("amogus.jpg"))
